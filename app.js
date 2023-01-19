319daa561731e86e9478aa47a787e1770e80594b
Christian Valeri <mortlaxris@outlook.com>
2023-01-18 21:03:48 -0600

    Have all docment.gets

---
+++ app.js
@@ -0,0 +1,31 @@
+const start = () =>{
+//Result document get
+    const result = document.getElementById(result-screen);
+//Btn document get
+    const btn1 = document.getElementById(one);
+    const btn2 = document.getElementById(two);
+    const btn3 = document.getElementById(three);
+    const btn4 = document.getElementById(four);
+    const btn5 = document.getElementById(five);
+    const btn6 = document.getElementById(six);
+    const btn7 = document.getElementById(seven);
+    const btn8 = document.getElementById(eight);
+    const btn9 = document.getElementById(nine);
+    const btn0 = document.getElementById(zero);
+
+    const btnClear = document.getElementById(AC);
+    const btnOPara = document.getElementById(o-para);
+    const btnCPara = document.getElementById(c-para);
+    const btnDot = document.getElementById(dot);
+    const btnDiv = document.getElementById(divide);
+    const btnPlus = document.getElementById(plus);
+    const btnMult = document.getElementById(multiply);
+    const btnMinus = document.getElementById(minus);
+    const btnPerc = document.getElementById(percentage);
+    const btnEqual = document.getElementById(equsl);
+
+    let resultScreen = 0;
+
+
+};
+start();
